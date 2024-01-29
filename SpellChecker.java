b3ace27e1f628c5705fae1fba235a6c6c8e83e13
ellabalter <ella.balter@post.runi.ac.il>
2024-01-29 20:21:20 +0200

    another try

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,66 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 1) {
+			str = "";
+		}
+		else {
+			str = str.substring(1);
+		}
+		return str ;
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		int x = levenshtein(word1, tail(word2));
+		int y = levenshtein(tail(word1), tail(word2));
+		int z = levenshtein(tail(word1), word2);
+		int a = word1.length();
+		int b = word2.length();
+		if( a == 0) {
+			return b;
+		}
+		else {
+			if( b == 0){
+				return a;
+			}
+			else{
+				if( word1.charAt(1) == word2.charAt(1)){
+					return levenshtein(tail(word1), tail(word2));
+				}
+				else {
+
+					return (1 + Math.min(x, Math.min(y, z)));
+				}
+			}
+
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
+		String word;
+		for(int i = 0; i < dictionary.length; i++) {
+			word = in.readLine();
+			dictionary[i] = word;
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String currentWord = "";
+		for(int i = 0; i < dictionary.length; i++){
+			if(levenshtein(currentWord, word) > levenshtein(word, dictionary[i]) && levenshtein(word, dictionary[i]) < threshold ) {
+				currentWord = dictionary[i];
+			}
+		}
+		if(currentWord.isEmpty()){
+			return word;
+		}
+		return currentWord;
 	}
 
 }
